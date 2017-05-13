fc2c35d703089dcdc241231affea9af6084c1a91
qsj <shujunqian@163.com>
2017-05-14 00:44:42 +0800

    路由时传递数参数

--- router/src/app/app-routing.module.ts
+++ router/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import{ Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ProductComponent} from "./product/product.component";
+import {Code404Component} from "./code404/code404.component";
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path:'product',
     component:ProductComponent
+  },
+  /*路由的通配符配置*/
+  {
+    path:'**',
+    component:Code404Component
   }
 ];
 
--- router/src/app/app.component.ts
+++ router/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  //
+  //toProductDetails 会用到一个路由对象  这个对象从构造函数里面过来
+  constructor(private router:Router){}
+
+  toProductDetails(){
+  //  导航到一个路由上（用router中的navigate方法）
+    this.router.navigate(['/product']);
+  }
+
+//  使用Angular Router
+
+//  在路由时传递数据
+//  1.在查询参数中传递数据
+//
 }
